8102902e3ef19952b3915ef07d19c15a0edff1fb
OmLotake15 <om.lotake15.thinkify@gmail.com>
2026-02-23 10:30:18 +0530

    Homepage done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,19 @@
+import React from "react";
+import "./Home.css";
+import kolhapuriImage from "../../assets/kolhapuri_homepage.png";
+
 function Home() {
   return (
-    <div>
-      <h1>This is Home Page</h1>
+    <div className="home-container">
+      <div className="image-wrapper">
+        <img
+          src={kolhapuriImage}
+          alt="Kolhapuri Hotel"
+          className="home-image"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
