a86f6cdb92a8a98b207a539e072d13b1cc727298
unknown <komal.more.thinkify@gmail.com>
2026-02-23 08:18:19 +0530

    Order page update

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
 import { Routes, Route } from "react-router-dom";
+
+import { useState } from "react";
+
 import Home from "./Pages/Home/Home";
 import Menu from "./Pages/Menu/Menu";
 import Order from "./Pages/Order/Order";
 import Profile from "./Pages/Profile/Profile";
 import Layout from "./Layout/Layout";
 
+
 function App() {
+const [cart, setCart] = useState([]);
+
+   const [billNo, setBillNo] = useState(() => {
+    const today = new Date().toLocaleDateString();
+    const savedDate = localStorage.getItem("billDate");
+    const savedBill = Number(localStorage.getItem("billNo")) || 0;
+
+    if (savedDate === today) {
+      return savedBill;
+    } else {
+      localStorage.setItem("billDate", today);
+      localStorage.setItem("billNo", 0);
+      return 0;
+    }
+  });
+
   return (
     <Routes>
+
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="menu" element={<Menu />} />
-        <Route path="order" element={<Order />} />
+        {/* <Route path="menu" element={<Menu />} />
+        <Route path="order" element={<Order />} /> */}
+          <Route path="menu" element={<Menu cart={cart} setCart={setCart} />}/>
+         <Route path="order" element={<Order cart={cart} setCart={setCart} billNo={billNo} setBillNo={setBillNo}/>}/>
         <Route path="profile" element={<Profile />} />
-      </Route>
+         </Route>
     </Routes>
   );
 }
